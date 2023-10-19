3f31b2f9de8134ff1f40a23342e545023a1167fa
Sakhile123 <115566123+Sakhile123@users.noreply.github.com>
2023-10-19 20:04:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -18,5 +18,5 @@ function showSlides() {
 
     slides[slideIndex - 1].style.display = "block";
 
-    setTimeout(showSlides, 3000); // Change image every 3 seconds (adjust this time as needed)
+    setTimeout(showSlides, 2000); // Change image every 3 seconds (adjust this time as needed)
 }
